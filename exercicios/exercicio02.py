db246d536f4fc5c51297ab7bc7c416f9b1d2e4da
Victor Ferreira Diniz <86570043+VictorFDiniz@users.noreply.github.com>
2024-11-23 20:19:20 -0300

    curso python com django

---
+++ exercicios/exercicio02.py
@@ -0,0 +1,11 @@
+nome = 'Luiz Otávio'
+altura =  1.80
+peso = 95
+imc = peso / (altura * altura) # IMC = peso / (altura x altura)
+
+print(nome, 'tem', 'de', altura)
+print('pesa', peso, 'quilos e seu IMC é', imc)
+
+# Luiz Otávio tem 1.80 de altura
+# pesa 95 quilos e seu IMC é
+# 29.320987654320987
---
+++ fundamentos/aula07.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print('Adicão', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2 #float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2 #inteiro
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2
+print('Módulo', modulo) 
