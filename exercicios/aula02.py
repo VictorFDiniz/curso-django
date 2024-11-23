49da686a253ff0f4c7859f22fad7290ae17d6b22
Victor Ferreira Diniz <86570043+VictorFDiniz@users.noreply.github.com>
2024-11-23 20:14:18 -0300

    curso python com django

---
+++ exercicios/aula02.py
@@ -0,0 +1,20 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+String são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+# Aspas simples
+print('Luiz Otávio')
+print(1, 'Luiz "Otávio"')
+
+# Aspas duplas
+print("Luiz Otávio")
+print(2, "Luiz 'Otávio'")
+
+# Escape
+print("Luiz \"Otávio\"")
